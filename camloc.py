7ab68cce219473fde916f534efea70cd8783cf52
Sushant Rao <sushantrao@umass.edu>
2017-10-29 02:21:31 -0400

    update

--- camloc.py
+++ camloc.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np 
 import matplotlib.pyplot as plt 
 
+"""
 #methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
  #           'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
 
@@ -21,3 +22,29 @@ for pt in zip(*loc[::-1]):
 plt.imshow(cv2.cvtColor(img_rgb, cv2.COLOR_BGR2RGB),  interpolation='bicubic')
 #plt.plot([50,100],[80,100], 'c', linewidth=5)
 plt.show()
+"""
+
+img1 = cv2.imread('Images/pattern.png',0) # queryImage
+img2 = cv2.imread('Images/IMG_6719',0)    # trainImage
+
+# Initiate SIFT detector
+orb = cv2.ORB()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = orb.detectAndCompute(img1,None)
+kp2, des2 = orb.detectAndCompute(img2,None)
+
+# create BFMatcher object
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+# Match descriptors.
+matches = bf.match(des1,des2)
+
+# Sort them in the order of their distance.
+matches = sorted(matches, key = lambda x:x.distance)
+
+# Draw first 10 matches.
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10], flags=2)
+
+plt.imshow(img3),plt.show()
+
