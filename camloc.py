b53e4bd64490d59a6d41e32138a2c61b1318ce3d
Sushant Rao <sushantrao@umass.edu>
2017-10-29 13:53:45 -0400

    updated to OpenCV3

--- camloc.py
+++ camloc.py
@@ -25,26 +25,29 @@ plt.show()
 """
 
 img1 = cv2.imread('Images/pattern.png',0) # queryImage
-img2 = cv2.imread('Images/IMG_6719',0)    # trainImage
+img_rgb = cv2.imread('Images/IMG_6719.JPG')
+img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+img2 = cv2.resize(img_gray,None,fx=0.35, fy=0.35, interpolation = cv2.INTER_LINEAR )
 
 # Initiate SIFT detector
-orb = cv2.ORB()
+orb = cv2.ORB_create()
 
 # find the keypoints and descriptors with SIFT
 kp1, des1 = orb.detectAndCompute(img1,None)
 kp2, des2 = orb.detectAndCompute(img2,None)
 
 # create BFMatcher object
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+bf = cv2.BFMatcher(cv2.NORM_HAMMING,crossCheck=True)
 
-# Match descriptors.
+# Match descriptors.        
 matches = bf.match(des1,des2)
 
 # Sort them in the order of their distance.
 matches = sorted(matches, key = lambda x:x.distance)
 
 # Draw first 10 matches.
-img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10], flags=2)
 
-plt.imshow(img3),plt.show()
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10],None, flags=2)
 
+plt.imshow(img3)
+plt.show()
