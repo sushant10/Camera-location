f67295757a2e74866787f0f82bcd00c416334209
Sushant Rao <sushantrao@umass.edu>
2017-10-31 02:02:08 -0400

    Camloc update
    Switched from orb to sift for feature matching and homography. Also
    using OpenCV 3.0.0

--- camloc.py
+++ camloc.py
@@ -23,19 +23,60 @@ plt.imshow(cv2.cvtColor(img_rgb, cv2.COLOR_BGR2RGB),  interpolation='bicubic')
 #plt.plot([50,100],[80,100], 'c', linewidth=5)
 plt.show()
 """
-#trial
+MIN_MATCH_COUNT = 10
+
 img1 = cv2.imread('Images/pattern.png',0) # queryImage
 img_rgb = cv2.imread('Images/IMG_6719.JPG')
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 img2 = cv2.resize(img_gray,None,fx=0.35, fy=0.35, interpolation = cv2.INTER_LINEAR )
 
 # Initiate SIFT detector
-orb = cv2.ORB_create()
+sift = cv2.xfeatures2d.SIFT_create()
 
 # find the keypoints and descriptors with SIFT
-kp1, des1 = orb.detectAndCompute(img1,None)
-kp2, des2 = orb.detectAndCompute(img2,None)
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
 
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks = 50)
+
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+matches = flann.knnMatch(des1,des2,k=2)
+
+# store all the good matches as per Lowe's ratio test.
+good = []
+for m,n in matches:
+    if m.distance < 0.7*n.distance:
+        good.append(m)
+if len(good)>MIN_MATCH_COUNT:
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+    matchesMask = mask.ravel().tolist()
+
+    h,w = img1.shape
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts,M)
+
+    img2 = cv2.polylines(img2,[np.int32(dst)],True,0,3, cv2.LINE_AA)
+
+else:
+    print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+    matchesMask = None
+
+draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
+
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+
+plt.imshow(img3, 'gray'),plt.show()
+
+"""
 # create BFMatcher object
 bf = cv2.BFMatcher(cv2.NORM_HAMMING,crossCheck=True)
 
@@ -51,3 +92,4 @@ img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10],None, flags=2)
 
 plt.imshow(img3)
 plt.show()
+"""
